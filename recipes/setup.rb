cc5216a46ccd47572b5ea5750d0ab8f0499b7f24
Troy <troyfredmist@gmail.com>
2017-09-19 20:59:45 +0000

    Template replaced the motd OHAI code

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -14,23 +14,33 @@ end
 package 'ntp'
 
 
+###THIS IS WITH THE ERB TEMPLATE #########
+template '/etc/motd' do
+	source 'motd.erb'
+	action :create
+# The source can be anything like index.html.erb or php.ini.erb etc
+
+end
 
 
-file '/etc/motd' do
-        content "This server is the property of GODMODE!.
-	HOSTNAME: #{node['hostname']}
-	IPADDRESS: #{node['ipaddress']}
-	CPU: #{node['cpu']['0']['mhz']}
-	MEMORY: #{node['memory']['total']}
+###THIS IS WITHOUT THE ERB TEMPLATE!!!###############
+#
+#file '/etc/motd' do
+ #       content "This server is the property of GODMODE!.
+#	HOSTNAME: #{node['hostname']}
+#	IPADDRESS: #{node['ipaddress']}
+#	CPU: #{node['cpu']['0']['mhz']}
+#	MEMORY: #{node['memory']['total']}
 
-"#the above code uses the ohai command to gather system data.
+#"
+#the above code uses the ohai command to gather system data.
 #syntax for getting the results is node['item']['any sub string']['more substrings']
 #surround them all in #{} so the system knows it is a variable outside the string
 
-        action :create
-        owner 'root'
-        group 'root'
-end
+#        action :create
+#        owner 'root'
+#        group 'root'
+#end
 
 service 'ntpd' do
         action [ :enable, :start]
