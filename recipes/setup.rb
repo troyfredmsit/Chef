1f00621a9ad6a05a276d811a40916272066ea320
Troy <troyfredmist@gmail.com>
2017-09-19 20:37:08 +0000

    Used OHAI and variable input to get local system data

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -13,8 +13,20 @@ end
 
 package 'ntp'
 
+
+
+
 file '/etc/motd' do
-        content 'This server is the property of GODMODE!.'
+        content "This server is the property of GODMODE!.
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	CPU: #{node['cpu']['0']['mhz']}
+	MEMORY: #{node['memory']['total']}
+
+"#the above code uses the ohai command to gather system data.
+#syntax for getting the results is node['item']['any sub string']['more substrings']
+#surround them all in #{} so the system knows it is a variable outside the string
+
         action :create
         owner 'root'
         group 'root'
