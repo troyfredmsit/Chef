116f2deddbe0e88417a969bb309c3fc42747b48e
vagrant <vagrant@localhost.localdomain>
2017-09-19 18:41:49 +0000

    initial git commit for the cookbook workstation

---
+++ recipes/setup.rb
@@ -0,0 +1,26 @@
+# ~/setup.rb
+package 'vim-enhanced'
+package 'nano'
+
+package 'git' do
+	action :install
+	
+end
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+	content 'This server is the property of GODMODE!.'
+	action :create
+	owner 'root'
+	group 'root'
+end
+
+service 'ntpd' do
+	action [ :enable, :start]
+
+end
+
